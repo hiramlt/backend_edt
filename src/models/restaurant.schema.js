5efe32f250cca74c34d44ab3dcfcb576c0b3510f
hiramlt <toledohiram2@gmail.com>
2024-05-03 15:23:04 -0600

    feat: create and update restaurant validations

---
+++ src/models/restaurant.schema.js
@@ -0,0 +1,121 @@
+import { z } from 'zod'
+
+export const RestaurantSchema = z.object({
+  rating: z
+    .number({
+      required_error: 'rating is required',
+      invalid_type_error: 'rating must be a number',
+    })
+    .int({ message: 'rating must be an integer' }),
+  name: z
+    .string({
+      required_error: 'name is required',
+      invalid_type_error: 'name must be a string',
+    })
+    .min(1, { message: 'name can not be empty' }),
+  site: z
+    .string({
+      required_error: 'site is required',
+      invalid_type_error: 'site must be a string',
+    })
+    .url({ message: 'site must be a valid URL' }),
+  email: z
+    .string({
+      required_error: 'email is required',
+      invalid_type_error: 'email must be a string',
+    })
+    .email({ message: 'email must have a valid format' }),
+  phone: z
+    .string({
+      required_error: 'phone is required',
+      invalid_type_error: 'phone must be a string',
+    })
+    .min(1, { message: 'phone can not be empty' }), // Regex validar numero si hay tiempo
+  street: z
+    .string({
+      required_error: 'street is required',
+      invalid_type_error: 'street must be a string',
+    })
+    .min(1, { message: 'street can not be empty' }),
+  city: z
+    .string({
+      required_error: 'city is required',
+      invalid_type_error: 'city must be a string',
+    })
+    .min(1, { message: 'city can not be empty' }),
+  state: z
+    .string({
+      required_error: 'state is required',
+      invalid_type_error: 'state must be a string',
+    })
+    .min(1, { message: 'state can not be empty' }),
+  lat: z.number({
+    required_error: 'lat is required',
+    invalid_type_error: 'lat must be a number',
+  }),
+  lng: z.number({
+    required_error: 'lng is required',
+    invalid_type_error: 'lng must be a number',
+  }),
+})
+
+export const UpdateRestaurantSchema = z.object({
+  rating: z
+    .number({
+      invalid_type_error: 'rating must be a number',
+    })
+    .int({ message: 'rating must be an integer' })
+    .optional(),
+  name: z
+    .string({
+      invalid_type_error: 'name must be a string',
+    })
+    .min(1, { message: 'name can not be empty' })
+    .optional(),
+  site: z
+    .string({
+      invalid_type_error: 'site must be a string',
+    })
+    .url({ message: 'site must be a valid URL' })
+    .optional(),
+  email: z
+    .string({
+      invalid_type_error: 'email must be a string',
+    })
+    .email({ message: 'email must have a valid format' })
+    .optional(),
+  phone: z
+    .string({
+      invalid_type_error: 'phone must be a string',
+    })
+    .min(1, { message: 'phone can not be empty' })
+    .optional(), // Regex validar numero si hay tiempo
+  street: z
+    .string({
+      invalid_type_error: 'street must be a string',
+    })
+    .min(1, { message: 'street can not be empty' })
+    .optional(),
+  city: z
+    .string({
+      invalid_type_error: 'city must be a string',
+    })
+    .min(1, { message: 'city can not be empty' })
+    .optional(),
+  state: z
+    .string({
+      invalid_type_error: 'state must be a string',
+    })
+    .min(1, { message: 'state can not be empty' })
+    .optional(),
+  lat: z
+    .number({
+      invalid_type_error: 'lat must be a number',
+    })
+    .optional(),
+  lng: z
+    .number({
+      invalid_type_error: 'lng must be a number',
+    })
+    .optional(),
+})
--- src/services/restaurants.service.js
+++ src/services/restaurants.service.js
@@ -1,8 +1,20 @@
 import RestaurantsRepository from '../repositories/restaurants.repository.js'
 import { InvalidDataException, NotFoundException } from '../utils/errors.js'
+import {
+  RestaurantSchema,
+  UpdateRestaurantSchema,
+} from '../models/restaurant.schema.js'
 
 export default class RestaurantsService {
   static async create(data) {
+    const result = RestaurantSchema.safeParse(data)
+    if (!result.success) {
+      const errors = result.error.errors.map((err) => {
+        return err.message
+      })
+      throw new InvalidDataException(`Missing restaurant data: [${errors}]`)
+    }
+
     return await RestaurantsRepository.create(data)
   }
 
@@ -21,10 +33,21 @@ export default class RestaurantsService {
   }
 
   static async update(id, data) {
-    const restaurant = await RestaurantsRepository.getById(id)
+    if (Object.keys(data).length === 0) {
+      throw new InvalidDataException('Missing restaurant data')
+    }
 
+    const restaurant = await RestaurantsRepository.getById(id)
     if (!restaurant) {
       throw new NotFoundException('Restaurant not found')
+    }
+
+    const result = UpdateRestaurantSchema.safeParse(data)
+    if (!result.success) {
+      const errors = result.error.errors.map((err) => {
+        return err.message
+      })
+      throw new InvalidDataException(`Missing restaurant data: [${errors}]`)
     }
 
     return await RestaurantsRepository.update(id, data)
